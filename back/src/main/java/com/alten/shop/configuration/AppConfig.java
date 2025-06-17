88f9e28358a49f180e128ce2f6dee5d07a63304c
ZaeRon007 <julien.pelissier8@orange.fr>
2025-06-17 14:47:53 +0200

    files update for back architecture

---
+++ back/src/main/java/com/alten/shop/configuration/AppConfig.java
@@ -0,0 +1,20 @@
+package com.alten.shop.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.io.FileSystemResource;
+
+@Configuration
+public class AppConfig {
+
+    /**
+     * Configure un autre emplacement contenant des propriétés à charger
+     */
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+        PropertySourcesPlaceholderConfigurer configurer = new PropertySourcesPlaceholderConfigurer();
+        configurer.setLocation(new FileSystemResource(".env"));
+        return configurer;
+    }
+}
