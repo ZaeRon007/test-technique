1546aabbb212e4dbea8e07a480e53b940c10dc50
ZaeRon007 <julien.pelissier8@orange.fr>
2025-06-24 17:19:22 +0200

    update for product component

--- front/src/app/altenShop/components/products/products.component.ts
+++ front/src/app/altenShop/components/products/products.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ProductService } from '../../services/productService';
+import { Subscription } from 'rxjs';
+import { productEntity } from '../../../core/models/productEntity';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrl: './products.component.scss'
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit, OnDestroy {
+  private sub: Subscription = new Subscription();
+  productsTab: productEntity[] = [];
 
+  constructor(private productService: ProductService){
+  }
+  ngOnInit(): void {
+    this.sub = this.productService.getAllProducts().subscribe((productsList: productEntity[]) => {
+      this.productsTab = productsList;
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
+  }
 }
---
+++ front/src/app/altenShop/services/productService.ts
@@ -0,0 +1,23 @@
+import { Injectable } from "@angular/core";
+import { environment } from "../../../environments/environment";
+import { BehaviorSubject, Observable, tap } from "rxjs";
+import { productEntity } from "../../core/models/productEntity";
+import { HttpClient } from "@angular/common/http";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class ProductService {
+    private apiUrl = environment.baseUrl;
+    private products$ = new BehaviorSubject<productEntity[]>([new productEntity])
+
+    constructor(private http: HttpClient){
+
+    }
+    
+    public getAllProducts(): Observable<productEntity[]>{
+        return this.http.get<productEntity[]>(`${this.apiUrl}products`).pipe(
+            tap(produit => this.products$.next(produit))
+        );
+    }
+}
